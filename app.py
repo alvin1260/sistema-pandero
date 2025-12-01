632f86e52201a6ecceb974572e99c7aebf4a06bb
alvin1260 <dickghm@gmail.com>
2025-12-01 12:43:14 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -246,7 +246,7 @@ elif st.session_state.rol == 'admin':
                     tag = '½' if r['Tipo']=='Medio' else ''
                     with st.expander(f"T{r['Turno']} | {'🔴' if deuda>0 else '🟢'} {r['Nombre']} {tag}"):
                         c1, c2 = st.columns([3,1])
-                        c1.write(f"DNI: {r['DNI']} | Deuda: {deuda}"); c1.markdown(f"[📲 WhatsApp](https://wa.me/?text=Hola%20{r['Nombre']})")
+                        c1.write(f"DNI: {r['DNI']} | Deuda: {deuda}"); c1.markdown(f"[📲 WhatsApp](https://wa.me/?text=Hola%20{r['Nombre']} necesitamos tu pago)")
                         c2.metric("Pagado", f"S/. {sum(c['Monto'] for c in cal if c['Estado']=='green')}")
                         dfv = pd.DataFrame(cal)[['Semana','Fecha','Monto','Estado']]
                         dfv['Monto'] = dfv['Monto'].apply(lambda x: f"S/. {x:.2f}")
